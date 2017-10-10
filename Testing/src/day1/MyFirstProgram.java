28e5586b166b55efdac1baf0d2ef6e673c937bc5
unknown <kavya.yelle@gmail.com>
2017-10-10 19:17:45 +0530

    upload

---
+++ Testing/src/day1/MyFirstProgram.java
@@ -0,0 +1,12 @@
+package day1;
+
+public class MyFirstProgram
+{
+
+	public static void main(String[] args)
+	{
+		System.out.print("This is my first program in Java    ");
+		System.out.println("Second line of the output");
+	}
+
+}
---
+++ Testing/src/day1/MySecondProgram.java
@@ -0,0 +1,12 @@
+package day1;
+
+public class MySecondProgram 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Second Program");
+		System.out.println("Second line of the output");
+	}
+
+}
